3a0be5b1e2e406851fef6a28abd3ba6e3a7aa75f
unknown <a_belkacem@esi.dz>
2024-10-04 15:40:02 +0200

    angle simulation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import {Routes} from '@angular/router';
 import {TemplateComponent} from "./core/template/container/template.component";
 import {ScpiComponent} from "./features/scpi/scpi.component";
 import {InvestComponent} from "./features/invest/invest.component";
-
+import { SimulationComponent } from './features/simulation/simulation.component';
 export const routes: Routes = [
 
   {
     path: '', component: TemplateComponent, children: [
       {path: 'scpi', component: ScpiComponent},
-      {path: 'invest', component: InvestComponent}
+      {path: 'invest', component: InvestComponent},
+      {path: 'simulation', component: SimulationComponent}
     ]
   }
 
