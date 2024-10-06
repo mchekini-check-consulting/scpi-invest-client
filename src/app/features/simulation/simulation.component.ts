20cdee3bcb7aa832d15c4eeea3ac1bafb3ed375f
unknown <a_belkacem@esi.dz>
2024-10-06 18:25:32 +0200

    angle simulation

--- src/app/features/simulation/simulation.component.ts
+++ src/app/features/simulation/simulation.component.ts
@@ -1,15 +1,52 @@
 import { Component } from '@angular/core';
 import { InputSwitchModule } from 'primeng/inputswitch';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+//...
+
 @Component({
+ 
   selector: 'app-simulation',
   standalone: true,
-  imports: [FormsModule, InputSwitchModule,ButtonModule,RippleModule],
+  imports: [FormsModule, InputSwitchModule,ButtonModule,RippleModule,SelectButtonModule,ReactiveFormsModule,CommonModule],
   templateUrl: './simulation.component.html',
   styleUrl: './simulation.component.css'
 })
 export class SimulationComponent {
-  checked: boolean = false;
+  formGroup!: FormGroup;
+
+    stateOptions: any[] = [
+        { label: 'non', value: 'non' },
+        { label: 'oui', value: 'oui' }
+    ];
+
+ 
+  
+    constructor(private router: Router) { }
+    ngOnInit() {
+        this.formGroup = new FormGroup({
+            value: new FormControl('oui')
+        });
+    }
+
+
+    title: string = 'TITRE';
+    isEditing: boolean = false;
+  
+    editTitle() {
+      this.isEditing = true; // Active le mode édition
+    }
+  
+    saveTitle() {
+      this.isEditing = false; // Désactive le mode édition
+    }
+    AjoutSCPI(){
+      console.log('nav')
+    //  this.router.navigate('/scpi');
+       this.router.navigateByUrl('/scpi');
+    }
 }
