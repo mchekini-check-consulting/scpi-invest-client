277a93e5e4fa5a68f036df479982af6ba6fa682b
Moh-Tacine <mytacine.dev@gmail.com>
2024-10-08 13:46:49 +0200

    Add property type

--- src/app/features/simulation/add-simulation-form/add-simulation-form.component.ts
+++ src/app/features/simulation/add-simulation-form/add-simulation-form.component.ts
@@ -1,5 +1,4 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {ScpiModel} from "../../../core/model/scpi.model";
 import {Scpi_itemComponent} from "../../scpi/components/scpi_item/scpi_item.component";
 import {MultiSelectModule} from "primeng/multiselect";
 import {FormsModule} from "@angular/forms";
@@ -7,10 +6,31 @@ import {InputNumberModule} from "primeng/inputnumber";
 import {ScpiDetailModel} from "../../../core/model/scpi-detail.model";
 import {NgIf} from "@angular/common";
 import {DropdownModule} from "primeng/dropdown";
+import {ScpiModel} from "../../../core/model/scpi.model";
 
-interface Sector {
-  name: string,
+
+enum property_type {
+  PLEINE_PROPRIETE,
+  NUE_PROPRIETE
 }
+interface Property {
+  type: property_type;
+  propertyLabel: string;
+}
+
+interface Stripping {
+  time: number;
+  percent : number;
+  stipLabel : string;
+}
+
+interface SimulatedScpi {
+  selectedProperty: Property;
+  totalInvest: number;
+  partNb: number;
+  monthlyIncomes: number;
+}
+
 @Component({
   selector: 'app-add-simulation-form',
   standalone: true,
@@ -29,36 +49,77 @@ export class AddSimulationFormComponent implements OnInit {
   @Input() scpiDetails!:ScpiDetailModel;
   @Input() scpi!:ScpiModel;
   @Output("onCloseForm") onCloseForm = new EventEmitter<boolean>();
+  // @Output("onAddScpi") onAddScpi = new EventEmitter<ScpiDetailModel>();
 
+  protected readonly property_type = property_type;
 
-  sector!: Sector[];
-  selectedSectors!: Sector[];
-  totalInvest:number = 0;
-  partNb:number = 0;
+  // Sector
+  properties!: Property[];
+  simulatedScpi!: SimulatedScpi;
+
+  // Delay
+  selectedStrip!: Stripping;
+  stripping!: Stripping[];
 
   ngOnInit(): void {
-    this.sector = [
-      {name: "Pleine propriété (Standard)"},
-      {name: "Nue-propriété (avancée)"},
+
+    this.properties = [
+      {propertyLabel: "Pleine propriété (Standard)", type: property_type.PLEINE_PROPRIETE},
+      {propertyLabel: "Nue-propriété (avancée)", type: property_type.NUE_PROPRIETE},
     ];
 
+    this.stripping = [
+      {time: 5, percent : 80},
+      {time: 6, percent : 77},
+      {time: 7, percent : 74},
+      {time: 8, percent : 71},
+      {time: 9, percent : 69},
+      {time: 10, percent : 67}
+    ].map(option => ({
+      ...option,
+      stipLabel : `${option.time} ans - ${option.percent}%`
+    }));
+
+    this.selectedStrip = this.stripping[0];
+
+
+    this.simulatedScpi = {
+      selectedProperty: this.properties[0],
+      totalInvest : 0,
+      partNb: 1,
+      monthlyIncomes : 0
+    };
   }
 
   onCancelBtnClick() {
     this.onCloseForm.emit(false);
-    this.totalInvest=0;
-    this.partNb=0;
+    this.simulatedScpi.totalInvest = 0;
+    this.simulatedScpi.partNb = 1;
+    this.simulatedScpi.selectedProperty = this.properties[0];
+    this.selectedStrip = this.stripping[0];
   }
 
-  calculateRevenus(partNumber: number): string {
+  calculateRevenus(partNumber: number): number {
   let partPrice = Object.values(this.scpiDetails.prices)[Object.values(this.scpiDetails.prices).length - 1];
 
-  let totalInvestissement = partPrice * partNumber;
-  this.totalInvest = totalInvestissement;
+  // part
+  let percentagePart = 1
+  if(this.simulatedScpi.selectedProperty.type === property_type.NUE_PROPRIETE)
+  {
+    percentagePart = this.selectedStrip.percent / 100;
+  }
 
-  let revenusGeneres = totalInvestissement *
+  this.simulatedScpi.totalInvest = partPrice * partNumber * percentagePart;
+
+    let revenusGeneres = this.simulatedScpi.totalInvest *
     Object.values(this.scpiDetails.distributionRate)[Object.values(this.scpiDetails.distributionRate).length - 1] / 100;
 
-  return (revenusGeneres / 12).toFixed(2);
+    this.simulatedScpi.monthlyIncomes = Number((revenusGeneres / 12).toFixed(2));
+
+    return this.simulatedScpi.monthlyIncomes;
+  }
+
+  onAddSimulatedScpi() {
+    console.log(this.simulatedScpi);
   }
 }
