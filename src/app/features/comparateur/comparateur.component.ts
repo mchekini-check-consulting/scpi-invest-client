8e90f48183679364c74dfe5f875de81764d8e017
Ouali Abdiche <oualiabdiche@outlook.fr>
2024-10-20 14:05:41 +0100

    Init comparateur

---
+++ src/app/features/comparateur/comparateur.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comparateur',
+  standalone: true,
+  imports: [],
+  templateUrl: './comparateur.component.html',
+  styleUrl: './comparateur.component.css'
+})
+export class ComparateurComponent {
+
+}
