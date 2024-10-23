3ffd40b86595a51a92080ef6ccacbb2f9e5dcfa4
Moh-Tacine <mytacine.dev@gmail.com>
2024-10-23 13:36:25 +0200

    Get user investments

--- src/app/features/invest/invest.component.ts
+++ src/app/features/invest/invest.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {TableModule} from "primeng/table";
-import {CurrencyPipe, DatePipe, NgForOf, NgIf, PercentPipe} from "@angular/common";
+import {CurrencyPipe, DatePipe, NgForOf, NgIf, NgOptimizedImage, PercentPipe} from "@angular/common";
 import {UserScpiModel} from "../../core/model/user-scpi.model";
 import {ScpiService} from "../../core/service/scpi.service";
 import {TabViewModule} from "primeng/tabview";
@@ -13,6 +13,8 @@ import {RatingModule} from "primeng/rating";
 import {TagModule} from "primeng/tag";
 import {StatChartComponent} from "./stat-chart/stat-chart.component";
 import {CardModule} from "primeng/card";
+import {PanelModule} from "primeng/panel";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-invest',
@@ -32,28 +34,84 @@ import {CardModule} from "primeng/card";
     TagModule,
     PercentPipe,
     StatChartComponent,
-    CardModule
+    CardModule,
+    PanelModule,
+    NgOptimizedImage
   ],
   templateUrl: './invest.component.html',
   styleUrl: './invest.component.css'
 })
 export class InvestComponent implements OnInit{
 
-  userScpiList!: UserScpiModel[];
+  scpiOwnedList!: UserScpiModel[];
+  scpiInPendingList!: UserScpiModel[];
 
-  constructor(private scpiService: ScpiService, private primengConfig: PrimeNGConfig) {
+  constructor(private router: Router, private scpiService: ScpiService, private primengConfig: PrimeNGConfig) {
   }
   ngOnInit(): void {
+
+    this.scpiOwnedList = [];
+    this.scpiInPendingList = [];
 
     this.primengConfig.ripple = true;
 
     this.scpiService.userScpiService().subscribe(data => {
-        this.userScpiList = data.map(item => {
-          return {
-            ...item,
-            transactionDate : new Date(item.transactionDate),
-          }
-        });
+
+      data.forEach(scpi => {
+        if(scpi.investmentStatus === 'PENDING') {
+          this.scpiInPendingList.push({
+            ...scpi,
+            investmentStatus: 'EN COURS',
+            image: "scpi-img/" + this.randomImage() + '.png'
+          });
+        }
+        else if(scpi.investmentStatus === 'VALIDATED') {
+          this.scpiOwnedList.push({
+            ...scpi,
+            image: "scpi-img/" + this.randomImage() + '.png'
+          });
+        }
+      })
     })
+  }
+
+  navigateToScpiList(): void {
+    this.router.navigate(['/scpi']);
+  }
+
+  getDetentionPeriod(requestDate: Date) {
+    const now = new Date();
+
+    const diffInMs = now.getTime() - new Date(requestDate).getTime();
+
+    const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+
+    const years = Math.floor(diffInDays / 365);
+    const months = Math.floor((diffInDays % 365) / 30);
+    const days = diffInDays % 30;
+
+    let result = '';
+
+    if(years > 0 && years < 2)
+      result += years + ' an';
+    if(years >= 2)
+      result += years + ' ans';
+
+    if(years > 0 && months > 0)
+      result += ' et '
+
+    if(months > 0)
+      result += months + ' mois';
+
+    if(months === 0 && years === 0 && days < 2)
+      result += '1 jour';
+    else if(months === 0 && years === 0 && days > 2)
+      result += days + ' jours';
+
+    return result;
+  }
+
+  randomImage():string {
+    return(Math.floor(Math.random() * 10) + 1).toString();
   }
 }
